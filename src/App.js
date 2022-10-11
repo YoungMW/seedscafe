a87500864945edc579da9b4b2590c58d44e0ea64
YoungMW <110984691+YoungMW@users.noreply.github.com>
2022-10-11 22:03:06 +0800

    initial commit. Nav Bar + Home Page + Modal (Nav Bar)

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import ModalNavBar from "./components/ModalNavBar";
+import NavBar from "./NavBar";
+import Home from "./pages/Home";
+
+function App() {
+  const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <div>
+      <div className="main--app--container">
+        {!openModal ? (
+          <>
+            <NavBar setOpenModal={setOpenModal} />
+            <Home />
+          </>
+        ) : (
+          <ModalNavBar setOpenModal={setOpenModal} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;

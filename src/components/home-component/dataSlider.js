fdf6475574f4e77e11f27c852ea5b7ddae258a9a
andikahadisaputra <andikahadisaputra@gmail.com>
2022-10-12 03:27:53 +0800

    add slider

---
+++ src/components/home-component/dataSlider.js
@@ -0,0 +1,23 @@
+import img1 from "../../assets/home/imgs/img1.png";
+import img2 from "../../assets/home/imgs/img2.png";
+import img3 from "../../assets/home/imgs/img3.png";
+
+const dataSlider = [
+  {
+    id: 1,
+    description: "Volunteering @ Rainbow Centre",
+    img: img1,
+  },
+  {
+    id: 2,
+    description: "Slider 2",
+    img: img2,
+  },
+  {
+    id: 3,
+    description: "Slider 3",
+    img: img3,
+  },
+];
+
+export default dataSlider;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,73 +5,78 @@ import { UilPalette } from "@iconscout/react-unicons";
 import { UilShoppingCartAlt } from "@iconscout/react-unicons";
 import { UilRainbow } from "@iconscout/react-unicons";
 import { UilUnamused } from "@iconscout/react-unicons";
+import Slider from "../components/home-component/Slider";
 
 const Home = () => {
   return (
-    <div className="main--home--container">
-      <div className="home--grid--box">
-        <div className="individual--grid--box">
-          <div className="individual--box--border">
-            <div className="text--logo--box">
-              <div className="individual--logo--circle">
-                <UilUtensils className="individual--logo" />
+    <>
+      <div className="main--home--container">
+        <div className="home--grid--box">
+          <div className="individual--grid--box">
+            <div className="individual--box--border">
+              <div className="text--logo--box">
+                <div className="individual--logo--circle">
+                  <UilUtensils className="individual--logo" />
+                </div>
+                <p className="individual--text">Menu</p>
               </div>
-              <p className="individual--text">Menu</p>
             </div>
           </div>
-        </div>
-        <div className="individual--grid--box">
-          <div className="individual--box--border">
-            <div className="text--logo--box">
-              <div className="individual--logo--circle">
-                <UilApps className="individual--logo" />
+          <div className="individual--grid--box">
+            <div className="individual--box--border">
+              <div className="text--logo--box">
+                <div className="individual--logo--circle">
+                  <UilApps className="individual--logo" />
+                </div>
+                <p className="individual--text">Venue Booking</p>
               </div>
-              <p className="individual--text">Venue Booking</p>
             </div>
           </div>
-        </div>
-        <div className="individual--grid--box">
-          <div className="individual--box--border">
-            <div className="text--logo--box">
-              <div className="individual--logo--circle">
-                <UilPalette className="individual--logo" />
+          <div className="individual--grid--box">
+            <div className="individual--box--border">
+              <div className="text--logo--box">
+                <div className="individual--logo--circle">
+                  <UilPalette className="individual--logo" />
+                </div>
+                <p className="individual--text">Support Artwork</p>
               </div>
-              <p className="individual--text">Support Artwork</p>
             </div>
           </div>
-        </div>
-        <div className="individual--grid--box">
-          <div className="individual--box--border">
-            <div className="text--logo--box">
-              <div className="individual--logo--circle">
-                <UilShoppingCartAlt className="individual--logo" />
+          <div className="individual--grid--box">
+            <div className="individual--box--border">
+              <div className="text--logo--box">
+                <div className="individual--logo--circle">
+                  <UilShoppingCartAlt className="individual--logo" />
+                </div>
+                <p className="individual--text">Support Micro Business</p>
               </div>
-              <p className="individual--text">Support Micro Business</p>
             </div>
           </div>
-        </div>
-        <div className="individual--grid--box">
-          <div className="individual--box--border">
-            <div className="text--logo--box">
-              <div className="individual--logo--circle">
-                <UilRainbow className="individual--logo" />
+          <div className="individual--grid--box">
+            <div className="individual--box--border">
+              <div className="text--logo--box">
+                <div className="individual--logo--circle">
+                  <UilRainbow className="individual--logo" />
+                </div>
+                <p className="individual--text">Rainbow Centre</p>
               </div>
-              <p className="individual--text">Rainbow Centre</p>
             </div>
           </div>
-        </div>
-        <div className="individual--grid--box">
-          <div className="individual--box--border">
-            <div className="text--logo--box">
-              <div className="individual--logo--circle">
-                <UilUnamused className="individual--logo" />
+          <div className="individual--grid--box">
+            <div className="individual--box--border">
+              <div className="text--logo--box">
+                <div className="individual--logo--circle">
+                  <UilUnamused className="individual--logo" />
+                </div>
+                <p className="individual--text">Donation</p>
               </div>
-              <p className="individual--text">Donation</p>
             </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <Slider />
+    </>
   );
 };
 
